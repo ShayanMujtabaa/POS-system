c1e55a40a584ee28eb947154c8e5357b43ba185a
Hassan <hassanaliwork888@gmail.com>
2024-12-29 09:50:50 +0500

    fixed indexing and merge

--- backend/index.js
+++ backend/index.js
@@ -32,6 +32,7 @@ app.use("/report", ReportsRoutes);
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+  });
 });
 
 app.get('/', POSroutes.GetTest);
