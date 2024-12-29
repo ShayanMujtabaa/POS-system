d476d908dc8b9844723992e86a0fc1c5712da339
ShayanMujtabaa <shayanisdon@gmail.com>
2024-12-29 11:07:34 +0500

    Added Delete Category (using ID)

--- frontend/src/components/redux/store.jsx
+++ frontend/src/components/redux/store.jsx
@@ -1,11 +1,13 @@
 import {configureStore} from "@reduxjs/toolkit";
 import cartSlice from './cartSlice';
 import ItemsSlice from './ItemsSlice';
+import CategoriesSlice from './CategoriesSlice';
 
 const store = configureStore({
     reducer: { 
         cart: cartSlice,
-        items: ItemsSlice
+        items: ItemsSlice,
+        categories: CategoriesSlice
     }, 
 
 });
