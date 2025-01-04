da7f343ed182454a3604cd2a7e2262bf96e8c64f
Hassan <hassanaliwork888@gmail.com>
2025-01-04 09:55:44 +0500

    improved skeleton a bit, still some work left

--- frontend/src/components/ItemsLoading.jsx
+++ frontend/src/components/ItemsLoading.jsx
@@ -1,46 +1,73 @@
-// SkeletonVariations.jsx
-import React from 'react';
-import { Skeleton } from './ui/skeleton';
+import React from "react";
+import { Skeleton } from "./ui/skeleton";
 
 const SkeletonVariations = ({ loading }) => {
   return (
     loading && (
-      <div className="flex gap-4">
+      <div className="flex flex-wrap">
         {/* Variation 1 */}
-        <Skeleton className="w-[300px] h-[70px] rounded-md">
-          <Skeleton className="w-[90px] h-[30px] rounded-md mt-2 ml-2" />
-          <Skeleton className="w-[120px] h-[20px] rounded-md mt-2 ml-2" />
-        </Skeleton>
+        <div className="w-full px-2 sm:w-1/1 md:w-1/2 lg:w-1/2 xl:w-1/4  flex-1 ">
+          <Skeleton className="w-[280px] h-[70px] pt-1 mt-6 rounded-lg p-2 m-1 shadow-md">
+            <Skeleton className="w-[50px] h-[20px] rounded-md mt-2 ml-2" />
+            <Skeleton className="w-[120px] h-[15px] rounded-md mt-2 ml-2" />
+          </Skeleton>
+        </div>
 
         {/* Variation 2 */}
-        <Skeleton className="w-[250px] h-[70px] rounded-md">
-          <Skeleton className="w-[100px] h-[25px] rounded-md mt-2 ml-2" />
-          <Skeleton className="w-[130px] h-[18px] rounded-md mt-2 ml-2" />
-        </Skeleton>
+        <div className="w-full px-2 sm:w-1/1 md:w-1/2 lg:w-1/2 xl:w-1/4  flex-1 ">
+          <Skeleton className="w-[280px] h-[70px] pt-1 mt-6 rounded-lg p-2 m-1 shadow-md">
+            <Skeleton className="w-[80px] h-[20px] rounded-md mt-2 ml-2" />
+            <Skeleton className="w-[140px] h-[15px] rounded-md mt-2 ml-2" />
+          </Skeleton>
+        </div>
 
         {/* Variation 3 */}
-        <Skeleton className="w-[320px] h-[80px] rounded-md">
-          <Skeleton className="w-[110px] h-[35px] rounded-md mt-2 ml-2" />
-          <Skeleton className="w-[140px] h-[22px] rounded-md mt-2 ml-2" />
-        </Skeleton>
+        <div className="w-full px-2 sm:w-1/1 md:w-1/2 lg:w-1/2 xl:w-1/4  flex-1 ">
+          <Skeleton className="w-[280px] h-[70px] pt-1 mt-6 rounded-lg p-2 m-1 shadow-md">
+            <Skeleton className="w-[90px] h-[20px] rounded-md mt-2 ml-2" />
+            <Skeleton className="w-[140px] h-[15px] rounded-md mt-2 ml-2" />
+          </Skeleton>
+        </div>
 
         {/* Variation 4 */}
-        <Skeleton className="w-[280px] h-[70px] rounded-md">
-          <Skeleton className="w-[80px] h-[28px] rounded-md mt-2 ml-2" />
-          <Skeleton className="w-[110px] h-[18px] rounded-md mt-2 ml-2" />
-        </Skeleton>
+        <div className="w-full px-2 sm:w-1/1 md:w-1/2 lg:w-1/2 xl:w-1/4  flex-1 ">
+          <Skeleton className="w-[280px] h-[70px] pt-1 mt-6 rounded-lg p-2 m-1 shadow-md">
+            <Skeleton className="w-[60px] h-[20px] rounded-md mt-2 ml-2" />
+            <Skeleton className="w-[110px] h-[15px] rounded-md mt-2 ml-2" />
+          </Skeleton>
+        </div>
 
         {/* Variation 5 */}
-        <Skeleton className="w-[300px] h-[75px] rounded-md">
-          <Skeleton className="w-[95px] h-[32px] rounded-md mt-2 ml-2" />
-          <Skeleton className="w-[125px] h-[19px] rounded-md mt-2 ml-2" />
-        </Skeleton>
+        <div className="w-full px-2 sm:w-1/1 md:w-1/2 lg:w-1/2 xl:w-1/4  flex-1 ">
+          <Skeleton className="w-[280px] h-[70px] pt-1 mt-6 rounded-lg p-2 m-1 shadow-md">
+            <Skeleton className="w-[65px] h-[20px] rounded-md mt-2 ml-2" />
+            <Skeleton className="w-[125px] h-[15px] rounded-md mt-2 ml-2" />
+          </Skeleton>
+        </div>
 
-        {/* Variation 6 */}
-        <Skeleton className="w-[350px] h-[90px] rounded-md">
-          <Skeleton className="w-[120px] h-[40px] rounded-md mt-2 ml-2" />
-          <Skeleton className="w-[150px] h-[15px] rounded-md mt-2 ml-2" />
-        </Skeleton>
+        {/* Variation 7 */}
+        <div className="w-full px-2 sm:w-1/1 md:w-1/2 lg:w-1/2 xl:w-1/4  flex-1 ">
+          <Skeleton className="w-[280px] h-[70px] pt-1 mt-6 rounded-lg p-2 m-1 shadow-md">
+            <Skeleton className="w-[70px] h-[20px] rounded-md mt-2 ml-2" />
+            <Skeleton className="w-[120px] h-[15px] rounded-md mt-2 ml-2" />
+          </Skeleton>
+        </div>
+
+        {/* Variation 8 */}
+        <div className="w-full px-2 sm:w-1/1 md:w-1/2 lg:w-1/2 xl:w-1/4  flex-1 ">
+          <Skeleton className="w-[280px] h-[70px] pt-1 mt-6 rounded-lg p-2 m-1 shadow-md">
+            <Skeleton className="w-[60px] h-[20px] rounded-md mt-2 ml-2" />
+            <Skeleton className="w-[170px] h-[15px] rounded-md mt-2 ml-2" />
+          </Skeleton>
+        </div>
+
+        {/* Variation 9 */}
+        <div className="w-full px-2 sm:w-1/1 md:w-1/2 lg:w-1/2 xl:w-1/4  flex-1">
+          <Skeleton className="w-[280px] h-[70px] pt-1 mt-6 rounded-lg p-2 m-1 shadow-md">
+            <Skeleton className="w-[160px] h-[20px] rounded-md mt-2 ml-2" />
+            <Skeleton className="w-[125px] h-[15px] rounded-md mt-2 ml-2" />
+          </Skeleton>
+        </div>
       </div>
     )
   );
